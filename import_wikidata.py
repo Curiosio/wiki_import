4c243ea6826a8037ec3f962495329bc0fdbcb6aa
mario <mario947@gmailc.com>
2020-02-22 17:48:29 +0000

    Normalize WD DB

--- import_wikidata.py
+++ import_wikidata.py
@@ -5,34 +5,40 @@ from collections import defaultdict
 import argparse
 import subprocess
 import json
+import os
+import re
 
 import psycopg2
 from psycopg2 import extras
 
+DATE_PARSE_RE = re.compile(r'([-+]?[0-9]+)-([0-9][0-9])-([0-9][0-9])T([0-9][0-9]):([0-9][0-9]):([0-9][0-9])Z?')
 
 def setup_db(connection_string):
   conn = psycopg2.connect(connection_string)
   cursor = conn.cursor()
-  cursor.execute('DROP TABLE IF EXISTS wikidata')
-  cursor.execute('CREATE TABLE wikidata ('
+  cursor.execute('CREATE SCHEMA wd;')
+  cursor.execute('DROP TABLE IF EXISTS wd.wikidata')
+  cursor.execute('CREATE TABLE wd.wikidata ('
                  '    wikipedia_id TEXT PRIMARY KEY,'
                  '    title TEXT,'
                  '    wikidata_id TEXT,'
                  '    description TEXT,'
+                 '    labels JSONB,'
+                 '    sitelinks JSONB,'
                  '    properties JSONB'
                  ')')
-  cursor.execute('CREATE INDEX wikidata_wikidata_id ON wikidata(wikidata_id)')
-  cursor.execute('CREATE INDEX wikidata_properties ON wikidata USING gin(properties)')
+
+  conn.commit()
   return conn, cursor
 
 
-def parse_wikidata(lines):
-  for line in lines:
-    line = line.strip()
+def parse_wikidata(line):
+
+    line = line.decode("utf-8").strip()
     if line and line[0] == '{':
       if line[-1] == ',':
         line = line[:-1]
-      yield json.loads(line)
+      return json.loads(line)
 
 
 def map_value(value, id_name_map):
@@ -74,41 +80,63 @@ def map_value(value, id_name_map):
   return None
 
 
-def main(dump, cursor):
+def main(dump, cursor, conn):
   """We do two scans:
      - first collect the id -> name / wikipedia title
      - then store the actual objects with a json property.
      The first step takes quite a bit of memory (5Gb) - could possibly be done using a temporary table in postgres.
   """
-  c = 0
-  skip = 0
   id_name_map = {}
-  for d in parse_wikidata(subprocess.Popen(['bzcat'], stdin=file(dump), stdout=subprocess.PIPE).stdout):
-    c += 1
-    if c % 1000 == 0:
-      print c, skip
-    if d.get('sitelinks') and d['sitelinks'].get('enwiki'):
-      value = d['sitelinks']['enwiki']['title']
-    elif d['labels'].get('en'):
-      value = id_name_map[d['id']] = d['labels']['en']['value']
-    else:
-      skip += 1
-      continue
-    id_name_map[d['id']] = value
+  if os.path.isfile('properties.json'):
+      print('loading properties from file')
+      id_name_map = json.load(open('properties.json'))
+  else:
+    c = 0
+    skip = 0
+    for line in subprocess.Popen(['bzcat'], stdin=open(dump, 'r'), stdout=subprocess.PIPE).stdout:
+        d = parse_wikidata(line)
+        if not d:
+            print('Failed to parse', line[0])
+            continue
+        c += 1
+        if c % 1000 == 0:
+          print(c, skip)
+        if d.get('sitelinks') and d['sitelinks'].get('enwiki'):
+          value = d['sitelinks']['enwiki']['title']
+        elif d['labels'].get('en'):
+          value = id_name_map[d['id']] = d['labels']['en']['value']
+        else:
+          skip += 1
+          continue
+        id_name_map[d['id']] = value
+
+    json.dump(id_name_map, open('properties.json', 'w'))
 
   wp_ids = set()
   c = 0
   rec = 0
   dupes = 0
-  for d in parse_wikidata(subprocess.Popen(['bzcat'], stdin=file(dump), stdout=subprocess.PIPE).stdout):
+  for line in subprocess.Popen(['bzcat'], stdin=open(dump, 'r'), stdout=subprocess.PIPE).stdout:
+    d = parse_wikidata(line)
+    if not d:
+        continue
     c += 1
     if c % 1000 == 0:
-      print c, rec, dupes
+      print(c, rec, dupes)
+    if c % 10000 == 0:
+      conn.commit()
+
+    labels = [d['labels'][x]['value'] for x in d.get('labels', {})]
+    sitelinks = [d.get('sitelinks')[x]['title'] for x in d.get('sitelinks', {})]
+
     wikipedia_id = d.get('sitelinks', {}).get('enwiki', {}).get('title')
     title = d['labels'].get('en', {}).get('value')
     description = d['descriptions'].get('en', {}).get('value')
     wikidata_id = d['id']
     properties = {}
+    properties['sitelinks'] = d.get('sitelinks')
+    properties['labels'] = d.get('labels')
+
     if wikipedia_id and title:
       # There are some duplicate wikipedia_id's in there. We could make wikidata_id the primary key
       # but that doesn't fix the underlying dupe
@@ -148,13 +176,13 @@ def main(dump, cursor):
 
 
       rec += 1
-      cursor.execute('INSERT INTO wikidata (wikipedia_id, title, wikidata_id, description, properties) VALUES (%s, %s, %s, %s, %s)',
-                     (wikipedia_id, title, wikidata_id, description, extras.Json(properties)))
+      cursor.execute('INSERT INTO wd.wikidata (wikipedia_id, title, wikidata_id, labels, sitelinks, description, properties) VALUES (%s, %s, %s, %s, %s, %s, %s)',
+              (wikipedia_id, title, wikidata_id, extras.Json(labels), extras.Json(sitelinks), description, extras.Json(properties)))
 
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Import wikidata into postgress')
-  parser.add_argument('--postgres', type=str,
+  parser.add_argument('postgres', type=str,
                       help='postgres connection string')
   parser.add_argument('dump', type=str,
                       help='BZipped wikipedia dump')
@@ -162,7 +190,52 @@ if __name__ == '__main__':
   args = parser.parse_args()
   conn, cursor = setup_db(args.postgres)
 
-  main(args.dump, cursor)
+  main(args.dump, cursor, conn)
 
+
+  cursor.execute('CREATE INDEX wd_wikidata_wikidata_id ON wd.wikidata(wikidata_id)')
+  cursor.execute('CREATE INDEX wd_wikidata_properties ON wd.wikidata USING gin(properties)')
+  cursor.execute('CREATE INDEX wd_wikidata_labels ON wd.wikidata USING gin(labels)')
+  cursor.execute('CREATE INDEX wd_wikidata_sitelinks ON wd.wikidata USING gin(sitelinks)')
+  conn.commit()
+  cursor.execute('DROP TABLE IF EXISTS wd.geo')
+  cursor.execute('CREATE TABLE wd.geo ('
+                 '    wikidata_id TEXT,'
+                 '    geometry geometry(POINT, 4326)'
+                 ')')
+  cursor.execute('INSERT into wd.geo (wikidata_id, geometry) '
+                'SELECT wikidata_id, ST_SETSRID(ST_MAKEPOINT((properties->\'coordinate location\'->>\'lng\')::DECIMAL, '
+                '(properties->\'coordinate location\'->>\'lat\')::DECIMAL), 4326) AS geometry '
+                'FROM wd.wikidata WHERE properties->\'coordinate location\' IS NOT NULL;'
+                )
+  cursor.execute('CREATE INDEX wd_geo_geometry ON wd.geo USING gist (geometry) TABLESPACE pg_default;')
+  conn.commit()
+  cursor.execute('DROP TABLE IF EXISTS wd.labels')
+  cursor.execute('CREATE TABLE wd.labels ('
+                 '    wikidata_id TEXT,'
+                 '    label TEXT'
+                 ')'
+                 )
+  cursor.execute('INSERT INTO wd.labels (wikidata_id, label) SELECT wikidata_id, jsonb_array_elements_text(labels) '
+                 'FROM wd.wikidata;'
+                )
+  cursor.execute('CREATE INDEX wd_wikidata_labels_trgm ON wd.labels USING gist (label COLLATE pg_catalog."default" gist_trgm_ops) TABLESPACE pg_default;')
   conn.commit()
 
+  cursor.execute('DROP TABLE IF EXISTS wd.instance')
+  cursor.execute('CREATE TABLE wd.instance ('
+                 '    wikidata_id TEXT,'
+                 '    instance_of TEXT'
+                 ');'
+                 )
+  cursor.execute('INSERT INTO wd.instance (wikidata_id, instance_of) '
+                 'SELECT wikidata_id, lower(properties->>\'instance of\')::jsonb '
+                 'FROM wd.wikidata WHERE jsonb_typeof(properties->\'instance of\') = \'array\';'
+                 )
+  cursor.execute('INSERT INTO wd.instance (wikidata_id, instance_of) '
+                 'SELECT wikidata_id, jsonb_build_array(lower(properties->>\'instance of\')) '
+                 'FROM wd.wikidata WHERE jsonb_typeof(properties->\'instance of\') = \'string\';'
+                )
+  cursor.execute('CREATE INDEX wd_wikidata_instance ON wd.instance USING gin (instance_of) TABLESPACE pg_default;')
+
+  conn.commit()
