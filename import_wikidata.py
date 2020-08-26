64812f48108eee417d12a393c05e436fae4a449d
mario947 <mario947@gmail.com>
2020-08-26 03:23:24 +0300

    Updated DB schema

--- import_wikidata.py
+++ import_wikidata.py
@@ -16,9 +16,9 @@ DATE_PARSE_RE = re.compile(r'([-+]?[0-9]+)-([0-9][0-9])-([0-9][0-9])T([0-9][0-9]
 def setup_db(connection_string):
   conn = psycopg2.connect(connection_string)
   cursor = conn.cursor()
-  cursor.execute('CREATE SCHEMA IF NOT EXISTS wd;')
-  cursor.execute('DROP TABLE IF EXISTS wd.wikidata')
-  cursor.execute('CREATE TABLE wd.wikidata ('
+  cursor.execute('CREATE SCHEMA IF NOT EXISTS import;')
+  cursor.execute('DROP TABLE IF EXISTS import.wikidata;')
+  cursor.execute('CREATE TABLE import.wikidata ('
                  '    wikipedia_id TEXT PRIMARY KEY,'
                  '    title TEXT,'
                  '    wikidata_id TEXT,'
@@ -26,7 +26,13 @@ def setup_db(connection_string):
                  '    labels JSONB,'
                  '    sitelinks JSONB,'
                  '    properties JSONB'
-                 ')')
+                 ');')
+  cursor.execute('DROP TABLE IF EXISTS import.id2name;')
+  cursor.execute('CREATE TABLE import.id2name ('
+                 '    id TEXT PRIMARY KEY,'
+                 '    title TEXT,'
+                 '    CONSTRAINT id2name_wikidata_id UNIQUE (id)'
+                 ');')
 
   conn.commit()
   return conn, cursor
@@ -176,7 +182,7 @@ def main(dump, cursor, conn):
 
 
       rec += 1
-      cursor.execute('INSERT INTO wd.wikidata (wikipedia_id, title, wikidata_id, labels, sitelinks, description, properties) VALUES (%s, %s, %s, %s, %s, %s, %s)',
+      cursor.execute('INSERT INTO import.wikidata (wikipedia_id, title, wikidata_id, labels, sitelinks, description, properties) VALUES (%s, %s, %s, %s, %s, %s, %s)',
               (wikipedia_id, title, wikidata_id, extras.Json(labels), extras.Json(sitelinks), description, extras.Json(properties)))
 
 
@@ -193,49 +199,52 @@ if __name__ == '__main__':
   main(args.dump, cursor, conn)
 
 
-  cursor.execute('CREATE INDEX wd_wikidata_wikidata_id ON wd.wikidata(wikidata_id)')
-  cursor.execute('CREATE INDEX wd_wikidata_properties ON wd.wikidata USING gin(properties)')
-  cursor.execute('CREATE INDEX wd_wikidata_labels ON wd.wikidata USING gin(labels)')
-  cursor.execute('CREATE INDEX wd_wikidata_sitelinks ON wd.wikidata USING gin(sitelinks)')
+  cursor.execute('CREATE INDEX wd_wikidata_wikidata_id ON import.wikidata(wikidata_id)')
+  cursor.execute('CREATE INDEX wd_wikidata_properties ON import.wikidata USING gin(properties)')
+  cursor.execute('CREATE INDEX wd_wikidata_labels ON import.wikidata USING gin(labels)')
+  cursor.execute('CREATE INDEX wd_wikidata_sitelinks ON import.wikidata USING gin(sitelinks)')
   conn.commit()
-  cursor.execute('DROP TABLE IF EXISTS wd.geo')
-  cursor.execute('CREATE TABLE wd.geo ('
+  cursor.execute('DROP TABLE IF EXISTS import.geo')
+  cursor.execute('CREATE TABLE import.geo ('
                  '    wikidata_id TEXT,'
-                 '    geometry geometry(POINT, 4326)'
+                 '    geometry geometry(POINT, 4326),'
+                 '    CONSTRAINT wd_geo_unique UNIQUE (wikidata_id)'
                  ')')
-  cursor.execute('INSERT into wd.geo (wikidata_id, geometry) '
+  cursor.execute('INSERT into import.geo (wikidata_id, geometry) '
                 'SELECT wikidata_id, ST_SETSRID(ST_MAKEPOINT((properties->\'coordinate location\'->>\'lng\')::DECIMAL, '
                 '(properties->\'coordinate location\'->>\'lat\')::DECIMAL), 4326) AS geometry '
-                'FROM wd.wikidata WHERE properties->\'coordinate location\' IS NOT NULL;'
+                'FROM import.wikidata WHERE properties->\'coordinate location\' IS NOT NULL;'
                 )
-  cursor.execute('CREATE INDEX wd_geo_geometry ON wd.geo USING gist (geometry) TABLESPACE pg_default;')
+  cursor.execute('CREATE INDEX wd_geo_geometry ON import.geo USING gist (geometry) TABLESPACE pg_default;')
   conn.commit()
-  cursor.execute('DROP TABLE IF EXISTS wd.labels')
-  cursor.execute('CREATE TABLE wd.labels ('
+  cursor.execute('DROP TABLE IF EXISTS import.labels')
+  cursor.execute('CREATE TABLE import.labels ('
                  '    wikidata_id TEXT,'
-                 '    label TEXT'
+                 '    label TEXT,'
+                 '    CONSTRAINT wd_label_unique UNIQUE (wikidata_id, label)'
                  ')'
                  )
-  cursor.execute('INSERT INTO wd.labels (wikidata_id, label) SELECT wikidata_id, jsonb_array_elements_text(labels) '
-                 'FROM wd.wikidata;'
+  cursor.execute('INSERT INTO import.labels (wikidata_id, label) SELECT wikidata_id, jsonb_array_elements_text(labels) '
+                 'FROM import.wikidata ON CONFLICT DO NOTHING;'
                 )
-  cursor.execute('CREATE INDEX wd_wikidata_labels_trgm ON wd.labels USING gist (label COLLATE pg_catalog."default" gist_trgm_ops) TABLESPACE pg_default;')
+  cursor.execute('CREATE INDEX wd_wikidata_labels_trgm ON import.labels USING gist (label COLLATE pg_catalog."default" gist_trgm_ops) TABLESPACE pg_default;')
   conn.commit()
 
-  cursor.execute('DROP TABLE IF EXISTS wd.instance')
-  cursor.execute('CREATE TABLE wd.instance ('
+  cursor.execute('DROP TABLE IF EXISTS import.instance')
+  cursor.execute('CREATE TABLE import.instance ('
                  '    wikidata_id TEXT,'
-                 '    instance_of TEXT'
+                 '    instance_of TEXT,'
+                 '    CONSTRAINT wd_instance_unique UNIQUE (wikidata_id)'
                  ');'
                  )
-  cursor.execute('INSERT INTO wd.instance (wikidata_id, instance_of) '
+  cursor.execute('INSERT INTO import.instance (wikidata_id, instance_of) '
                  'SELECT wikidata_id, lower(properties->>\'instance of\')::jsonb '
-                 'FROM wd.wikidata WHERE jsonb_typeof(properties->\'instance of\') = \'array\';'
+                 'FROM import.wikidata WHERE jsonb_typeof(properties->\'instance of\') = \'array\';'
                  )
-  cursor.execute('INSERT INTO wd.instance (wikidata_id, instance_of) '
+  cursor.execute('INSERT INTO import.instance (wikidata_id, instance_of) '
                  'SELECT wikidata_id, jsonb_build_array(lower(properties->>\'instance of\')) '
-                 'FROM wd.wikidata WHERE jsonb_typeof(properties->\'instance of\') = \'string\';'
+                 'FROM import.wikidata WHERE jsonb_typeof(properties->\'instance of\') = \'string\';'
                 )
-  cursor.execute('CREATE INDEX wd_wikidata_instance ON wd.instance USING gist (instance_of COLLATE pg_catalog."default" gist_trgm_ops) TABLESPACE pg_default;')
+  cursor.execute('CREATE INDEX wd_wikidata_instance ON import.instance USING gist (instance_of COLLATE pg_catalog."default" gist_trgm_ops) TABLESPACE pg_default;')
 
   conn.commit()
